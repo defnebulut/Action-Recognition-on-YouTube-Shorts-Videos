67e31cee3dd94d7d351e8aabb06ce6f85caf1b20
Defne Bulut <86167833+defnebulut@users.noreply.github.com>
2024-09-21 19:17:27 +0300

    Deleted unnecessary file

--- train/yolo/config.py
+++
@@ -1,7 +0,0 @@
-DATASET_DIR = "UCF-9"
-CLASSES = ["ApplyEyeMakeup", "BasketballDunk", "Diving", "IceDancing", "HorseRiding", "PlayingGuitar",
-           "BlowingCandles", "WalkingWithDog"]
-IMAGE_HEIGHT, IMAGE_WIDTH = 64, 64
-SEQUENCE_LENGTH = 5
-
-
